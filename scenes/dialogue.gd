277936dbbd216fc55aee062b1b316bafb8349f2e
Hanako256 <nala.gilmour@gmail.com>
2026-01-16 19:09:35 -0700

    FIGURED OUT TYPEWRITER
    let's go chat

--- scenes/dialogue.gd
+++ scenes/dialogue.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var text: String = "Yep, I'm P03. OK?"
+var txt: String = ""
 var tween: Tween = create_tween()
 
 func _ready():
@@ -7,8 +7,5 @@ func _ready():
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("hit_note"):
-		$DialogueText.clear()
-		$Typewriter.clear()
-		$Typewriter.add_text("Okay, I'm done my gloating!")
-		tween.tween_property($Typewriter, "visible_ratio", 1.0, 2.0).from(0.0)
-		#$Typewriter.add_text("It's always been 'P03 do this!' 'P03 do that!' Well, now, who's the tutorial man? Who's the tutorial man? It's me.")
+		$Typewriter.typing_speed = 20.0
+		$Typewriter.typewrite("It's always been 'P03 do this!' 'P03 do that!' Well now who's the tutorial man? Who's the tutorial man? It's me golden freddy.")
